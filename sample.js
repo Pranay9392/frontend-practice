d8a17ee1b61b7ea43e28d401eda9e4559cd55aea
Pranay9392 <aletipranaypranay@gmail.com>
2025-05-16 13:46:05 +0530

    learning java script

---
+++ sample.js
@@ -0,0 +1,56 @@
+console.log('Welcome pranay')
+
+var a1 = 10;
+let b1 = 20;
+const c1 = 30;
+
+console.log("print var variable",a1)
+console.log("print var variable",b1)
+console.log("print var variable",c1)
+
+var fristname = 'pranay'
+let lastname = 'aletti'
+console.log(fristname,lastname)
+
+
+
+var a = 10;
+var b = "10";
+if (a==b){
+    console.log("values are same");
+}else{
+    console.log("values are differnet");
+}
+
+
+if (a === b){
+    console.log("values are same and datatypes are same also")
+}else{
+    console.log("values are same but dataypes are diffferent")
+}
+
+
+
+console.log("1 -10 numbers:")
+for (let i = 1 ; i< 11;i++){
+    if (i%2 == 0){console.log(i);}
+    
+}
+
+var day = 3
+
+switch(day){
+    case 1:
+        console.log('Sunday')
+        break
+    case 2:
+        console.log('Monday')
+        break
+    default:
+        console.log('Weekend')
+
+}
+
+
+
+
